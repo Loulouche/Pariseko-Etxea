135af350e481023577c8d08ce636aa1d992ddd90
Louise DELARUE <louise.delarue@epita.fr>
2025-07-04 23:17:44 +0200

    oush

---
+++ public/honoraire.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page d'accueil</title>
+    <link rel="stylesheet" href="css/hono-style.css">
+</head>
+<body>
+
+<!-- Header -->
+<header class="header">
+    <!-- Logo -->
+    <div class="logo">
+        <img src="../asset/logo.png" alt="PARISEKO ETXEA" class="logo-img">
+    </div>
+
+    <!-- Navigation -->
+    <nav class="nav">
+        <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="biens.php">Biens</a></li>
+            <li><a href="honoraire.php">Honoraire</a></li>
+            <li><a href="about.php">À propos</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="login.php">Login</a></li>
+        </ul>
+    </nav>
+</header>
